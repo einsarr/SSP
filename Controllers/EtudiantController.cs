06c2cd1e0611df570642db79f6d83cab5016b3cd
Moussa SARR <moussa.sarr@education.sn>
2020-11-09 22:07:08 +0000

    first

---
+++ Controllers/EtudiantController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ServerSideProcessing.Models;
+
+namespace ServerSideProcessing.Controllers
+{
+    public class EtudiantController : Controller
+    {
+        DBEcoleEntities db = new DBEcoleEntities();
+        // GET: Etudiant
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetList()
+        {
+            List<Etudiants> etudiants = new List<Etudiants>();
+            etudiants = db.Etudiants.ToList();
+            return Json(new { data = etudiants }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
